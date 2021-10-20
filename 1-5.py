4e016f0af9b167d89ca1ab69cb41b19e42b6d617
Alex Gilvarry <agilvarry@utah.gov>
2021-10-20 12:29:12 -0600

    1-5 init

---
+++ 1-5.py
@@ -0,0 +1,13 @@
+def knapsack():
+    """
+    knapsack([1,2,5,9,10], 22)
+    a) [10, 9, 5, 2, 1]
+    b) [1,2,5,9,10]
+    c) [10, 9, 5, 2, 1]
+    """
+    print("hi")
+    
+    return
+
+if __name__ == "__main__":
+    knapsack()
