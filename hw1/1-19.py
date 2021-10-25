bb5f4a9ed6eecb7688235f2a79838dd564c618b8
Alex Gilvarry <agilvarry@utah.gov>
2021-10-25 15:25:03 -0600

    ch1 ?s

--- hw1/1-19.py
+++ hw1/1-19.py
@@ -1,4 +1,7 @@
 """
-Prove by induction that a tree with nvertices has exactly n-1 edges
+Prove by induction that a tree with n vertices has exactly n-1 edges
 """
+#0 verticies, 0 edges
+# 1 vertices -(1)- 2 edges
 
+#therefore n verticies n-1 edges
--- hw1/1-22.py
+++ hw1/1-22.py
@@ -1,3 +1,5 @@
 """
 How many words are in the textbook?
 """
+
+#11 on a line x 77 lines == 777 *793 pages = ~616,161 words
