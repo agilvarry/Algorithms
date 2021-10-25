42345908f324cab3a4337bff90267ff3e6af0be6
Alex Gilvarry <agilvarry@utah.gov>
2021-10-25 15:43:27 -0600

    1 done

--- hw1/1-19.py
+++ hw1/1-19.py
@@ -1,7 +1,7 @@
 """
 Prove by induction that a tree with n vertices has exactly n-1 edges
 """
-#0 verticies, 0 edges
-# 1 vertices -(1)- 2 edges
+#1 verticies, 0 edges
+# 2 vertices 1 edge to connect them 
 
 #therefore n verticies n-1 edges
--- hw1/1-24.py
+++ hw1/1-24.py
@@ -1,4 +1,4 @@
 """Estimate how many cities are towns are in the United States"""
 
 #towns/cities in utah = 253
-#253*50 = 12,git 
+#253*50 = 12,000
