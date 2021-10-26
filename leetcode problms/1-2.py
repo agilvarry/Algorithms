108fa9fa7d219bb33b5063b477bf5cdcdcc6900c
Alex Gilvarry <agilvarry@utah.gov>
2021-10-26 16:04:22 -0600

    progress

--- leetcode problms/1-2.py
+++ leetcode problms/1-2.py
@@ -1,1 +1,31 @@
 #https://leetcode.com/problems/rotate-list/
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        
+        def frontToBack(head: Optional[ListNode]):
+            def iter(head: Optional[ListNode]):
+                
+                next = head.next
+                if next.next is None:
+                    head.next = None
+                    return head, next
+                else:
+                    head, next = iter(next)
+                return head, next  
+            
+            head, next = iter(head)
+            next.next = head
+            print(head)
+            
+            return next
+                
+        if k > 0:
+            head = frontToBack(head)
+            k = k-1
+        return head    
+        
