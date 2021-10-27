36c3a9ace4ebf691a8c5e188ce52d53ae854a055
Alex Gilvarry <agilvarry@utah.gov>
2021-10-27 11:18:25 -0600

    better solution

--- leetcode problms/1-2.py
+++ leetcode problms/1-2.py
@@ -4,8 +4,12 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    # my first attemps: i think this is n * k, worst case n*n
+    
     def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        """
+        my first attempt, tweaked to look aa bit better based on the source from betterRotate.
+        i think this is n * k, worst case n*n
+        """
         if not head or k == 0:
             return head
         
@@ -29,4 +33,30 @@ class Solution:
         for _ in range(k % count):
             head = rotate(head)
         return head
-    
+
+
+    def betterRotate(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        """
+        in this solution we turn the list into a continuous loop
+        then we find the item that should be the last, and set it's next->None
+        found the idea from: https://www.youtube.com/watch?v=fqZYZK3OCXQ
+        """
+        if not head or k == 0:
+            return head
+        
+        last = head
+        count = 1
+        while last.next:
+            count +=1
+            last = last.next
+
+        last.next = head
+        k = count- (k % count)-1
+        current = head
+
+        while k >0:
+            current = current.next
+            k=k-1
+        r = current.next
+        current.next= None
+        return r
