51da1f5d142e47a17179cfd23710404474096b2e
Alex Gilvarry <agilvarry@utah.gov>
2021-10-28 10:50:03 -0600

    1-3

--- leetcode problms/1-3.py
+++ leetcode problms/1-3.py
@@ -1,1 +1,22 @@
 #https://leetcode.com/problems/wiggle-sort-ii/
+#basically fast but not very memory efficient
+class Solution:
+    def wiggleSort(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        nums.sort()
+        l = len(nums)
+        m = l // 2
+        if l%2 == 1:
+            m+=1
+        
+        
+        small = nums[:m]
+        large = nums[m:]
+        
+        for i in range(0, len(nums)):
+            if i %2==0:
+                nums[i] = small.pop(-1)
+            else:
+                nums[i] = large.pop(-1)
