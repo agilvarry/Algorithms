0ee0f06eac4604b773f7a101f716674fdb0fd274
Alex Gilvarry <31046803+agilvarry@users.noreply.github.com>
2021-11-04 10:00:04 -0600

    test

--- leetcode problms/2-1.py
+++ leetcode problms/2-1.py
@@ -1,1 +1,24 @@
 #https://leetcode.com/problems/remove-k-digits/
+
+class Solution:
+    def removeKdigits(self, num: str, k: int) -> str:
+        if len(num) == k:
+            return "0"
+        
+        num_list = []
+        
+        for n in num:
+            while num_list and k>0 and num_list[-1] >n:
+                k -= 1
+                num_list.pop()
+            num_list.append(n)
+            
+            
+        while k>0:
+            num_list.pop()
+            k-=1
+            
+        while num_list[0] == "0" and len(num_list)>1:
+            num_list.pop(0)
+            
+        return "".join(num_list)
