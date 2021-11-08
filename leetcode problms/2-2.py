e2c67757bb12ded17732c6e936d19ba7b64a4bdb
Alex Gilvarry <31046803+agilvarry@users.noreply.github.com>
2021-11-08 09:43:00 -0700

    2-2

--- leetcode problms/2-2.py
+++ leetcode problms/2-2.py
@@ -1,1 +1,25 @@
 #https://leetcode.com/problems/counting-bits/
+class Solution:
+    """
+    This should help:
+    https://realpython.com/python-bitwise-operators/
+    """   
+    def countBits(self, n: int) -> List[int]:
+                 
+        ones = []
+        for i in range(n+1):
+            count = self.count_ones(i)
+            ones.append(count)  
+        return ones
+    
+    def count_ones(self, n) -> int:
+        """
+        here we drop bits from the input number, and use a bitwise and to see if the right most
+        bit is a 1, incrementing count as we go
+        """
+        count = 0
+        while n:
+            count +=  n & 1 #bitwise AND only returns a 1 when there is a 1 in the right most binary representation of n
+            n >>= 1 #we're dropping the right most bit off the number here
+        return count 
+        
