584d34610bcb9f99b889caba84d5bd99015b58ff
Alex Gilvarry <31046803+agilvarry@users.noreply.github.com>
2021-11-15 11:17:36 -0700

    4-1

--- leetcode problms/2-3.py
+++ leetcode problms/2-3.py
@@ -1,1 +1,2 @@
 #https://leetcode.com/problems/4sum/
+#https://leetcode.com/problems/4sum/discuss/737096/Sum-MegaPost-Python3-Solution-with-a-detailed-explanation
--- leetcode problms/3-3.py
+++ leetcode problms/3-3.py
@@ -1,1 +1,1 @@
-https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/
+#https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/
