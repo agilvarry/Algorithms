c50054cc31fd1773128241682df9c46b11f68151
Alex Gilvarry <31046803+agilvarry@users.noreply.github.com>
2021-11-17 15:40:37 -0700

    2-3

--- leetcode problms/2-3.py
+++ leetcode problms/2-3.py
@@ -9,7 +9,37 @@ class Solution:
             else:
                 seen[n] = i
 #https://leetcode.com/problems/3sum/
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        seen = {}
+        res = []
+        #similar to original twoSum, but we collect all sums that equal target, not the idx
+        for i, n in enumerate(nums):
+            m = target - n
+            if m in seen:
+                res.append([m, n])
+            else:
+                seen[n] = i
+        return res  
+                
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        res = []
+        for i in range(len(nums)-2):
+            if i>0 and nums[i] == nums[i-1]: #this checks if the value is the same as the previous in the sorted list
+                continue
+            o = self.twoSum(nums[i+1:],-nums[i]) #target is negative of current num, so if we reach 0 in 2sum that's an answer
+            if o ==[]:
+                continue
+            else:
+                for idx in o:
+                    idx.append(nums[i])
+                    res.append(idx)
+        out = []  
+        #filter out any duplicate lists
+        for idx in res: 
+            if idx not in out:
+                out.append(idx)
+        return out
 
-#https://leetcode.com/problems/4sum/
-#https://leetcode.com/problems/4sum/discuss/737096/Sum-MegaPost-Python3-Solution-with-a-detailed-explanation
 
