8121dd242f1278de510a3189b97e9210f09de4ba
Alex Gilvarry <31046803+agilvarry@users.noreply.github.com>
2021-11-17 15:21:53 -0700

    2sum3sum

--- leetcode problms/2-3.py
+++ leetcode problms/2-3.py
@@ -1,2 +1,15 @@
+#https://leetcode.com/problems/two-sum/
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        seen = {}
+        for i, n in enumerate(nums):
+            m = target - n
+            if m in seen:
+                return [seen[m], i]
+            else:
+                seen[n] = i
+#https://leetcode.com/problems/3sum/
+
 #https://leetcode.com/problems/4sum/
 #https://leetcode.com/problems/4sum/discuss/737096/Sum-MegaPost-Python3-Solution-with-a-detailed-explanation
+
