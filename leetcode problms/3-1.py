90077dfaada736b0fe7d6b3e2206cb7e6a297de4
Alex Gilvarry <31046803+agilvarry@users.noreply.github.com>
2021-11-30 09:52:33 -0700

    3-1

--- leetcode problms/3-1.py
+++ leetcode problms/3-1.py
@@ -1,1 +1,13 @@
 #https://leetcode.com/problems/validate-binary-search-tree/
+
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:               
+        return self.recurse(root, float('-inf'), float('inf'))
+    
+    def recurse(self,  root, minV, maxV):
+        if root is None:
+            return True
+        elif root.val <= minV or root.val >= maxV:
+            return False
+        else:
+            return self.recurse(root.left, minV, root.val) and self.recurse(root.right, root.val, maxV)
