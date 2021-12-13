4d32dab0bd61172451e245030e1e51901817754d
Alex Gilvarry <31046803+agilvarry@users.noreply.github.com>
2021-12-13 10:29:52 -0700

    alternate way

--- contains_duplicate.py
+++ contains_duplicate.py
@@ -1,4 +1,3 @@
-#https://leetcode.com/problems/contains-duplicate/
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         seen = {} #dictionary passes but list doesn't because hack table access is faster than array access?
@@ -9,4 +8,11 @@ class Solution:
                 seen[i] = 1
         
         return False
+    def containsDuplicateSort(self, nums: List[int]) -> bool:
+        nums.sort() #hash table is faster, sorting the array uses less space 
+        for i in range(0, len(nums)-1):
+            if nums[i] == nums[i+1]:
+                return True
+        return False
+        
             
