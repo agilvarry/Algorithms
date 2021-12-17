1dd02e466abbd81ad0ace4a4cc4fb3ee1aa2bad9
Alex Gilvarry <31046803+agilvarry@users.noreply.github.com>
2021-12-17 13:49:17 -0700

    rotate array

---
+++ exercises/rotate_array.py
@@ -0,0 +1,11 @@
+#https://leetcode.com/problems/rotate-array/
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        
+        while k>0:
+            nums.insert(0,nums[-1])
+            nums.pop()
+            k -= 1
