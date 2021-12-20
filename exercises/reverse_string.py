6ecd30227cfdf80e32f19768b009b7c03c83c06e
Alex Gilvarry <31046803+agilvarry@users.noreply.github.com>
2021-12-20 10:59:22 -0700

    reverse string

---
+++ exercises/reverse_string.py
@@ -0,0 +1,9 @@
+#https://leetcode.com/problems/reverse-string/
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        low = 0
+        high = len(s)-1        
+        while low < high:
+            s[low], s[high] = s[high], s[low]
+            low += 1
+            high -= 1
