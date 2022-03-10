700ce6d903ffecc54808e39aa8a097d8139a632b
Alex Gilvarry <31046803+agilvarry@users.noreply.github.com>
2022-03-10 10:04:56 -0700

    DS1 done

---
+++ Data Structures I/lowest_common_ancestor.py
@@ -0,0 +1,11 @@
+#https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if root.val < p.val and root.val < q.val:
+            return self.lowestCommonAncestor(root.right, p, q)
+        elif root.val > p.val and root.val > q.val:
+            return self.lowestCommonAncestor(root.left, p, q)
+        else:
+            return root
+            
